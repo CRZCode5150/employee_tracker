290c6c09e0c82738a9d1f8b95b5445226ef0db21
Chris <chrisramirez15@yahoo.com>
2022-03-16 19:45:09 -0500

    added schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO departments (name)
+VALUES
+('Managment'),
+('Sales'),
+('HR'),
+('IT'),
+('Support');
+
+INSERT INTO roles (title)
+VALUES
+('CEO'),
+('Sales Manager'),
+('Sales Rep'),
+('HR Lead'),
+('HR Associate'),
+('IT Manager'),
+('IT Associate'),
+('Support Lead'),
+('Support Associate');
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+('Chris', 'Ramirez', 1),
+('Bob', 'Gild', 2),
+('Josh', 'Donald', 3),
+('Belle', 'Garcia', 4),
+('Alex', 'Bents', 5),
+('Dame', 'Garza', 6),
+('John', 'Kicken', 7),
+('Ruben', 'Rodz', 8),
+('Skyler', 'Grey', 9),
+('Paul', 'Mock', 10),
+('Chi', 'Bege', 11);
